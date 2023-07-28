e277f8e44546d845ac2302e18cfdb2b57592b916
JewishLewish <lanbrokhman@gmail.com>
2023-07-28 11:28:08 -0700

    Config File + Bytedance Sonic

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module polyconnect
 go 1.19
 
 require (
+	github.com/bytedance/sonic v1.9.2 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/klauspost/compress v1.10.3 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
 )
--- main.go
+++ main.go
@@ -3,18 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/bytedance/sonic"
 	"nhooyr.io/websocket"
 )
 
+// Settings.json parsing
+type settings struct {
+	Addr string `json:"addr"`
+	Port string `json:"port"`
+}
+
 func main() {
 
-	set := "0.0.0.0:25565"
+	set := portgrab()
 
 	http.HandleFunc("/ws", datahandler)
-	fmt.Print("Server started on -> "+set, "\n")
+	fmt.Print("Server started on -> "+set.Addr+":"+set.Port, "\n")
 
-	http.ListenAndServe(set, nil)
+	http.ListenAndServe(set.Addr+":"+set.Port, nil)
 }
 
 func datahandler(w http.ResponseWriter, r *http.Request) {
@@ -22,4 +30,37 @@ func datahandler(w http.ResponseWriter, r *http.Request) {
 	ws, _ := websocket.Accept(w, r, nil)
 	defer ws.Close(websocket.StatusNormalClosure, "")
 
+}
+
+func portgrab() settings {
+	if _, err := os.Stat("settings.json"); os.IsNotExist(err) {
+		setup()
+	}
+
+	if _, err := os.Stat("databases"); os.IsNotExist(err) {
+		err = os.Mkdir("databases", 0755)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Folder 'databases' created successfully.")
+	}
+
+	var set settings
+	sonic.Unmarshal(getFilecontent("settings.json"), set)
+	return set
+}
+
+func setup() {
+	defaultset := settings{
+		Addr: "0.0.0.0",
+		Port: "25565",
+	}
+	data, _ := sonic.ConfigFastest.MarshalIndent(&defaultset, "", "    ")
+	os.WriteFile("settings.json", data, 0644)
+	fmt.Print("Settings.json has been setup. \n")
+}
+
+func getFilecontent(filename string) []byte {
+	file, _ := os.ReadFile("settings.json")
+	return file
 }
